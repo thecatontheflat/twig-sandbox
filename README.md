b42d06806bc0a6fb2c4d5d6c38685493dd0485bc
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-04-09 21:44:35 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+twig-sandbox
+============
+
+A Symfony project created on April 9, 2015, 9:32 pm.
---
+++ src/Acme/DemoBundle/Controller/TwigController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class TwigController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/internal-controller", name="internalControllerDemo")
+     * @Template()
+     */
+    public function internalControllerDemoAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/internal-controller-included", name="includedInternalController")
+     * @Template()
+     */
+    public function includedInternalControllerAction()
+    {
+        return [];
+    }
+}
